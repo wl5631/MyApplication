9231ce2a20516266d05d2a284307a7eee91c3605
류강냉 <wuslco@naver.com>
2015-12-02 22:32:13 +0900

    add

--- adapter/src/main/java/com/example/adapter/MainActivity.java
+++ adapter/src/main/java/com/example/adapter/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,15 +35,17 @@ public class MainActivity extends AppCompatActivity {
         public MyGridAdapter(Context c) {
             context = c;
         }
-
+        String[] posterName = {"Sunny","Punch","The Host","Radio Star"," King and the clown","Maundy Thursday","The Prists","The Beauty Inside",
+                "Sunny","Punch","The Host","Radio Star"," King and the clown","Maundy Thursday","The Prists","The Beauty Inside",
+                "Sunny","Punch","The Host","Radio Star"," King and the clown","Maundy Thursday","The Prists","The Beauty Inside"};
 
         Integer[] posterID = {R.drawable.poster1, R.drawable.poster2, R.drawable.poster3, R.drawable.poster4,
-                R.drawable.poster6,R.drawable.poster1, R.drawable.poster2, R.drawable.poster3, R.drawable.poster4,
-                R.drawable.poster6,R.drawable.poster1, R.drawable.poster2, R.drawable.poster3, R.drawable.poster4,
-                R.drawable.poster6,R.drawable.poster1, R.drawable.poster2, R.drawable.poster3, R.drawable.poster4,
-                R.drawable.poster6};
-
-        String[] posterName = {"Sunny", "wanduk","Monster","RadioStar","King's man"};
+                R.drawable.poster6,R.drawable.poster5, R.drawable.poster7, R.drawable.poster9,
+                R.drawable.poster1, R.drawable.poster2, R.drawable.poster3, R.drawable.poster4,
+                R.drawable.poster6,R.drawable.poster5, R.drawable.poster7, R.drawable.poster9,
+                R.drawable.poster1, R.drawable.poster2, R.drawable.poster3, R.drawable.poster4,
+                R.drawable.poster6,R.drawable.poster5, R.drawable.poster7, R.drawable.poster9
+             };
 
         @Override
         public int getCount() {
@@ -62,31 +65,29 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageview = new ImageView(context);
-            imageview.setLayoutParams(new GridView.LayoutParams(150, 150));
-            imageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            imageview.setPadding(5, 5, 5, 5);
 
-            imageview.setImageResource(posterID[position]);
-
+            View grid = getLayoutInflater().inflate(R.layout.gridview, null);
+            ImageView image = (ImageView) grid.findViewById(R.id.imageView);
+            TextView tv = (TextView) grid.findViewById(R.id.textView);
+            image.setImageResource(posterID[position]);
+            tv.setText(posterName[position]);
 
             final int pos = position;
-            imageview.setOnClickListener(new View.OnClickListener() {
+            image.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     View dialogView = (View) View.inflate(MainActivity.this, R.layout.dialog, null);
                     AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
                     ImageView ivPoster = (ImageView) dialogView.findViewById(R.id.ivPoster);
                     ivPoster.setImageResource(posterID[pos]);
-                    dlg.setTitle("Big Poster");
-                    //아이콘넣자
+                    dlg.setTitle(posterName[pos]);
                     dlg.setView(dialogView);
                     dlg.setNegativeButton("Close", null);
                     dlg.show();
                 }
             });
 
-            return imageview;
+            return grid;
         }
     }
 }
